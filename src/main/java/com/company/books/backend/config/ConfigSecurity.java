edaefe2504c7a1f12377c1869e1e0d8d24224d92
CTS059-ADavila <andres.davila@cetustech.co>
2024-05-07 17:00:42 -0500

    creation file configSecurity and manager jwt

--- src/main/java/com/company/books/backend/config/ConfigSecurity.java
+++ src/main/java/com/company/books/backend/config/ConfigSecurity.java
@@ -1,57 +1,115 @@
 package com.company.books.backend.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.company.books.backend.filter.JwtReqFilter;
 @Configuration
 public class ConfigSecurity {
+    @Autowired
+    @Lazy
+    private JwtReqFilter jwtReqFilter;
+
+    /*
+     * @Bean
+     * public InMemoryUserDetailsManager userDetailsManager() {
+     * UserDetails alfredo = User.builder()
+     * .username("alfredo")
+     * .password("{noop}alfredo123")
+     * .roles("Empleado")
+     * .build();
+     * 
+     * UserDetails agustin = User.builder()
+     * .username("agustin")
+     * .password("{noop}agustin123")
+     * .roles("Empleado","Jefe")
+     * .build();
+     * 
+     * UserDetails edita = User.builder()
+     * .username("edita123")
+     * .password("{noop}edita123")
+     * .roles("Empleado","Jefe")
+     * .build();
+     * 
+     * return new InMemoryUserDetailsManager(alfredo,agustin,edita);
+     * 
+     * }
+     */
+
+    /*
+     * //Deprecado
+     * 
+     * @Bean
+     * public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+     * 
+     * http.authorizeRequests((authorizeHttpRequests) -> authorizeHttpRequests
+     * .requestMatchers(HttpMethod.GET, "/v1/libros").hasRole("Empleado")
+     * .requestMatchers(HttpMethod.GET, "/v1/libros/**").hasRole("Empleado")
+     * .requestMatchers(HttpMethod.POST, "/v1/libros/**").hasRole("Jefe")
+     * .requestMatchers(HttpMethod.PUT, "/v1/libros/**").hasRole("Jefe")
+     * .requestMatchers(HttpMethod.DELETE, "/v1/libros/**").hasRole("Jefe"));
+     * 
+     * http.httpBasic(Customizer.withDefaults());
+     * http.csrf((crsf) -> crsf.disable());
+     * return http.build();
+     * 
+     * };
+     */
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails andres = User.builder()
-                .username("andres_david")
-                .password("{noop}andres_david_123")
-                .roles("Empleado")
-                .build();
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
 
-        UserDetails mari = User.builder()
-                .username("mari_123")
-                .password("{noop}mari_123")
-                .roles("Empleado", "Jefe")
-                .build();
+    };
 
-        UserDetails juan = User.builder()
-                .username("juan_123")
-                .password("{noop}juan_123")
-                .roles("Empleado")
-                .build();
-
-        return new InMemoryUserDetailsManager(andres, mari, juan);
-
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
-    @SuppressWarnings("deprecation")
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // https://www.bezkoder.com/websecurityconfigureradapter-deprecated-spring-boot/
 
-        http.authorizeRequests((configure) -> {
-            configure
-            .requestMatchers(HttpMethod.GET, "/v1/libros").hasRole("Empleado")
-            .requestMatchers(HttpMethod.GET, "/v1/libros/**").hasRole("Empleado")
-            .requestMatchers(HttpMethod.POST, "/v1/libros/**").hasRole("Jefe")
-            .requestMatchers(HttpMethod.PUT, "/v1/libros/**").hasRole("Jefe")
-            .requestMatchers(HttpMethod.DELETE, "/v1/libros/**").hasRole("Jefe");
-        });
+        http.authorizeHttpRequests(
+                (requests) -> {
+                    requests.requestMatchers(HttpMethod.GET, "/v1/libros").hasRole("Empleado")
+                            .requestMatchers(HttpMethod.GET, "/v1/libros/**").hasRole("Empleado")
+                            .requestMatchers(HttpMethod.POST, "/v1/libros/**").hasRole("Jefe")
+                            .requestMatchers(HttpMethod.PUT, "/v1/libros/**").hasRole("Jefe")
+                            .requestMatchers(HttpMethod.DELETE, "/v1/libros/**").hasRole("Jefe")
+                            .requestMatchers(HttpMethod.GET, "/v1/categorias").hasRole("Empleado")
+                            .requestMatchers(HttpMethod.GET, "/v1/categorias/**").hasRole("Empleado")
+                            .requestMatchers(HttpMethod.POST, "/v1/categorias/**").hasRole("Jefe")
+                            .requestMatchers(HttpMethod.PUT, "/v1/categorias/**").hasRole("Jefe")
+                            .requestMatchers(HttpMethod.DELETE, "/v1/categorias/**").hasRole("Jefe")
+                            .requestMatchers("/v1/authenticate", "/v3/api-docs/**",
+                            "/swagger-ui/**",
+                            "/swagger-ui.html")
+                            .permitAll().anyRequest().authenticated();
+                            
+                })
+                .addFilterBefore(jwtReqFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.httpBasic(Customizer.withDefaults());
-        http.csrf((crsf)->crsf.disable());
+        http.csrf(csrf -> csrf.disable());
         return http.build();
 
-    }
+    };
 }
---
+++ src/main/java/com/company/books/backend/response/TokenResponse.java
@@ -0,0 +1,19 @@
+package com.company.books.backend.response;
+
+public class TokenResponse {
+    private String jwToken;
+
+    public TokenResponse(String jwToken) {
+        this.jwToken = jwToken;
+    }
+
+    public String getJwToken() {
+        return jwToken;
+    }
+
+    public void setJwToken(String jwToken) {
+        this.jwToken = jwToken;
+    }
+    
+
+}
